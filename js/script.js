14d51b1d3a97f0d466abc3c8846e7a21dd8b93a7
Mosiur Rahman <mosiurrahman123654@gmail.com>
2022-04-24 03:18:24 +0600

    Validation And Alart Is Done

--- js/script.js
+++ js/script.js
@@ -24,16 +24,27 @@ class UI{
         row.innerHTML = `<td>${book.title}</td>
         <td>${book.author}</td>
         <td>${book.isbn}</td>
-        <td><a style="color:red;text-Decoration:none;" href="#" class="delete">x</a></td>`;
-        // document.querySelector("a").style.textDecoration = "none";
-        // document.querySelector("a").style.color = "red";
+        <td><a href="#" class="delete">x</a></td>`;
+        
         list.appendChild(row);
     }
-
+// After Taking Value Remove Filds
     clearFilds(){
         document.querySelector("#title").value = "";
         document.querySelector("#author").value = "";
         document.querySelector("#isbn").value = "";
+    }
+// For Alart
+    showAlart(message, cassName){
+            let container = document.querySelector(".container");
+            let div = document.createElement("div");
+            div.className = ` alert ${cassName}`;
+            div.appendChild(document.createTextNode(message));
+            container.insertBefore(div, form);
+// Remove Alart
+            setTimeout(() =>{
+                document.querySelector(".alert").remove();
+            }, 3000);
     }
 }
 
@@ -46,11 +57,16 @@ function newBook (e){
     let title = document.querySelector("#title").value,
     author = document.querySelector("#author").value,
     isbn = document.querySelector("#isbn").value;
-
-    let book = new Book (title, author, isbn);
-    
     let ui = new UI();
-    ui.addToBookList(book);
-    ui.clearFilds();
+
+    if (title === "" || author === "" || isbn === ""){
+        ui.showAlart("Please fill all the fields!", "error");
+    }else{
+        let book = new Book (title, author, isbn);
+        ui.addToBookList(book);
+        ui.clearFilds();
+        ui.showAlart("Book Added!", "succes");
+    }
+    
     e.preventDefault();
 }
